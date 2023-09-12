4eb57b52142cc4950ac3f1c6a2069a54e9ad82b5
Shina <53410646+s7tya@users.noreply.github.com>
2023-09-12 02:15:41 +0000

    WIP: セッションの作成フォームを実装

---
+++ src/app/sessions/new/page.tsx
@@ -0,0 +1,98 @@
+"use client"
+
+import { Course } from "@/types/models";
+import { MeiliSearch } from 'meilisearch'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { css } from "../../../../styled-system/css";
+
+
+export default function CreateNewSessionPage() {
+  const { register, handleSubmit, watch, formState: { errors }, setError } = useForm<{ courseCode: string }>();
+  const [course, setCourse] = useState<Course | null>(null)
+
+  const client = new MeiliSearch({
+    host: process.env.NEXT_PUBLIC_MEILISEARCH_URL!,
+    apiKey: process.env.NEXT_PUBLIC_MEILISEARCH_API_KEY!,
+  })
+
+  const getCourse = async ({ courseCode }: { courseCode: string }) => {
+
+    await fetch(`${process.env.NEXT_PUBLIC_MEILISEARCH_URL!}/indexes/courses/documents/${courseCode}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_MEILISEARCH_API_KEY!}`
+      },
+    }).then(async res => {
+      if (res.status == 200) {
+        const course: Course = await res.json()
+        setCourse(course)
+      } else {
+        setError("courseCode", { type: "custom", message: "授業が見つかりません" })
+      }
+    })
+  }
+
+  return (
+    <main className={css({
+      maxWidth: "600px",
+      marginInline: "auto",
+      py: "20px",
+      display: "grid",
+      gap: "20px",
+    })}>
+      <div>
+        <p>講義コードを入力してください</p>
+        <form onSubmit={handleSubmit(getCourse)} className={css({
+          display: "flex",
+          gap: "4px",
+        })}>
+          <input placeholder="講義コード" {...register("courseCode")} className={css({
+            border: "1px solid",
+            borderColor: "gray.200",
+            p: "4px",
+            rounded: "md",
+          })} />
+          <button className={css({
+            background: "green.400",
+            rounded: "md",
+            py: "4px",
+            px: "12px",
+          })}>取得</button>
+
+        </form>
+        {errors.courseCode && (<p className={css({ color: "red.500" })}>{errors.courseCode?.message}</p>)}
+      </div>
+
+      {course && (
+        <>
+          <div className={css({
+          })}>
+            <p className={css({
+              fontSize: "xl",
+              fontWeight: "bold",
+            })}>{course.name}</p>
+            <p>{course.instructors}</p>
+          </div>
+
+          <form className={css({
+            display: "grid",
+            gap: "8px"
+          })}>
+            <input type="file" />
+            <button className={css({
+              py: "4px",
+              px: "8px",
+              display: "block",
+              textAlign: "center",
+              w: "full",
+              background: "green.400",
+              rounded: "md",
+              cursor: "pointer",
+            })}>作成</button>
+          </form>
+        </>
+      )
+      }
+    </main>
+  )
+}
