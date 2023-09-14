5337b94242eb143008e57f45ab2a114eb3127106
Shina <53410646+s7tya@users.noreply.github.com>
2023-09-14 11:33:55 +0000

    ファイルを分割

---
+++ src/app/sessions/[sessionId]/LiveChat.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { useForm } from "react-hook-form";
+import { useMutation, useStorage } from "../../../../liveblocks.config";
+import { LiveObject } from "@liveblocks/client";
+import { Comment } from "./Comment";
+
+export const LiveChat = () => {
+  const comments = useStorage((root) => root.comments);
+  const { register, handleSubmit, reset } = useForm<{ name: string, message: string }>()
+
+  const createNewComment = useMutation(
+    ({ storage }, author, message) => {
+      storage.get("comments").push(new LiveObject({
+        author,
+        message
+      }))
+    },
+    []
+  )
+
+  return (
+    <div>
+      <div>
+        <div>
+          {
+            comments.map((comment, idx) => {
+              return (
+                <Comment comment={comment} key={idx} />
+              )
+            })
+          }
+        </div>
+      </div>
+      <form onSubmit={handleSubmit((data) => {
+        createNewComment(data.name, data.message)
+        reset()
+      })}>
+        <input hidden value={"User"} {...register("name")} />
+        <input {...register("message")} />
+        <button type="submit">Post</button>
+      </form>
+    </div >
+  )
+}
