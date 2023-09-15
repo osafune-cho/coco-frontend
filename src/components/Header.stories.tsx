b05a90177882166351d78d8d188148e6c4f3f0e3
iori0705 <iodere77@gmail.com>
2023-09-15 09:50:27 +0900

    storybook

---
+++ src/components/Header.stories.tsx
@@ -0,0 +1,9 @@
+import { Header } from './Header'
+import { css } from "../../styled-system/css"
+
+export default {
+  title: 'Header',
+  component: Header,
+};
+
+export const HelloButton = () => <Header />
